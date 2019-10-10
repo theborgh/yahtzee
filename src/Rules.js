1f8d4e72549c60fa1f0a69e3c8728bf5ac90fe13
theborgh <borghiborghi@gmail.com>
2019-10-10 19:45:09 +0200

    Fix locked roll bug

--- src/Rules.js
+++ src/Rules.js
@@ -57,14 +57,27 @@ class SumDistro extends Rule {
 
 /** Check if full house (3-of-kind and 2-of-kind) */
 
-class FullHouse {
-  // TODO
+class FullHouse extends Rule {
+  evalRoll = dice => {
+    const freqs = this.freq(dice);
+    return freqs.includes(2) && freqs.includes(3) ? this.score : 0;
+  };
 }
 
 /** Check for small straights. */
 
-class SmallStraight {
-  // TODO
+class SmallStraight extends Rule {
+  evalRoll = dice => {
+    const d = new Set(dice);
+
+    if (
+      (d.has(2) && d.has(3) && d.has(4) && (d.has(1) || d.has(5))) ||
+      (d.has(3) && d.has(4) && d.has(5) && (d.has(2) || d.has(6)))
+    ) {
+      return this.score;
+    }
+    return 0;
+  };
 }
 
 /** Check for large straights. */
@@ -88,29 +101,29 @@ class Yahtzee extends Rule {
 }
 
 // ones, twos, etc score as sum of that value
-const ones = new TotalOneNumber({ val: 1 });
-const twos = new TotalOneNumber({ val: 2 });
-const threes = new TotalOneNumber({ val: 3 });
-const fours = new TotalOneNumber({ val: 4 });
-const fives = new TotalOneNumber({ val: 5 });
-const sixes = new TotalOneNumber({ val: 6 });
+const ones = new TotalOneNumber({val: 1});
+const twos = new TotalOneNumber({val: 2});
+const threes = new TotalOneNumber({val: 3});
+const fours = new TotalOneNumber({val: 4});
+const fives = new TotalOneNumber({val: 5});
+const sixes = new TotalOneNumber({val: 6});
 
 // three/four of kind score as sum of all dice
-const threeOfKind = new SumDistro({ count: 3 });
-const fourOfKind = new SumDistro({ count: 4 });
+const threeOfKind = new SumDistro({count: 3});
+const fourOfKind = new SumDistro({count: 4});
 
 // full house scores as flat 25
-const fullHouse = "TODO";
+const fullHouse = new FullHouse({score: 25});
 
 // small/large straights score as 30/40
-const smallStraight = "TODO";
-const largeStraight = new LargeStraight({ score: 40 });
+const smallStraight = new SmallStraight({score: 30});
+const largeStraight = new LargeStraight({score: 40});
 
 // yahtzee scores as 50
-const yahtzee = new Yahtzee({ score: 50 });
+const yahtzee = new Yahtzee({score: 50});
 
 // for chance, can view as some of all dice, requiring at least 0 of a kind
-const chance = new SumDistro({ count: 0 });
+const chance = new SumDistro({count: 0});
 
 export {
   ones,
@@ -125,5 +138,5 @@ export {
   smallStraight,
   largeStraight,
   yahtzee,
-  chance
+  chance,
 };
