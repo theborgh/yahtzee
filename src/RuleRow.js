a326c67c0c7ece98cd522659f11d75d870d4ec5c
theborgh <borghiborghi@gmail.com>
2019-10-10 20:15:13 +0200

    Fix styling and rescoring bug

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
-import './RuleRow.css'
+import React, {Component} from 'react';
+import './RuleRow.css';
 
 class RuleRow extends Component {
   render() {
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+      <tr
+        className={`RuleRow RuleRow-${
+          this.props.score === undefined ? 'active' : 'disabled'
+        }`}
+        onClick={this.props.score === undefined ? this.props.doScore : null}
+      >
+        <td className='RuleRow-name'>{this.props.name}</td>
+        <td className='RuleRow-score'>{this.props.score}</td>
       </tr>
-    )
+    );
   }
 }
 
